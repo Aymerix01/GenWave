8ef256f44e512c406b437e7fbe1a6340d9666ac3
Aymerix01 <aymerix.lacroix@gmail.com>
2023-04-27 09:11:36 +0200

    Version 2

--- js/index.js
+++ js/index.js
@@ -1,6 +1,41 @@
 const menuHamburger = document.querySelector(".BHamburger");
 const navLinks = document.querySelector(".toolbar")
 
+const firstp = document.querySelector('.first-page');
+const secondp = document.querySelector('.second-page');
+const thirdp = document.querySelector('.third-page');
+const fourthp = document.querySelector('.fourth-page');
+
+const scrollBtnAccueil = document.querySelector('.accueil');
+const scrollBtnpresentation = document.querySelector('.presentation');
+const scrollBtnLogiciel = document.querySelector('.logiciel');
+const scrollBtncontact = document.querySelector('.contact');
+
 menuHamburger.addEventListener('click', ()=> {
     navLinks.classList.toggle('mobile-menu')
 })
+
+scrollBtnAccueil.addEventListener('click', () => {
+  window.scrollTo({
+    top: firstp.offsetTop,
+    behavior: 'smooth'
+  });
+});
+scrollBtnpresentation.addEventListener('click', () => {
+    window.scrollTo({
+      top: secondp.offsetTop,
+      behavior: 'smooth'
+    });
+  });
+  scrollBtnLogiciel.addEventListener('click', () => {
+    window.scrollTo({
+      top: thirdp.offsetTop,
+      behavior: 'smooth'
+    });
+  });
+  scrollBtncontact.addEventListener('click', () => {
+    window.scrollTo({
+      top: fourthp.offsetTop,
+      behavior: 'smooth'
+    });
+  });
